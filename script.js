afbcf7d32b8f662e3e3f9cbd7be7d11cac279b15
alexnorrell <norrell.alex@gmail.com>
2023-01-10 17:25:26 -0500

    adding first vars

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ]; 
+var specialCharacter = ['~', '`', '!', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', ',', '?', '.', ];
+ 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,4 +17,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-testing
